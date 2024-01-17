96d706d20c779a7b532bc23a7d5771e7db0404d3
raimeiga <yusuke612@hotmail.com>
2024-01-17 13:16:42 +0900

    10.3 トップページのビューを編集/1. ToDoの追加用ボタンと追加用モーダル/

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -38,7 +38,7 @@
                  <div class="col">     
                      <div class="card bg-light">
                          <div class="card-body d-flex justify-content-between align-items-center">
-                             <h4 class="card-title ms-1 mb-0">{{ $goal->title }}</h4>
+                             <h4 class="card-title ms-1 mb-0">{{ $goal->title }}</h3>
                              <div class="d-flex align-items-center">     
                                   <a href="#" class="px-2 fs-5 fw-bold link-dark text-decoration-none" data-bs-toggle="modal" data-bs-target="#addTodoModal{{ $goal->id }}">＋</a>                                                                
                                 <div class="dropdown">
