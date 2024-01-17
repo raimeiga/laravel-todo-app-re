2667e2f2394d088edd03a935a3997fcc03d95b49
raimeiga <yusuke612@hotmail.com>
2024-01-17 13:19:33 +0900

    10.3 トップページのビューを編集/2. ToDoの「編集」「削除」リンクと編集・削除用モーダル

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -51,6 +51,31 @@
                                  </div>
                              </div>
                          </div>
+
+                         @foreach ($goal->todos()->orderBy('done', 'asc')->get() as $todo) 
+                             <!-- ToDoの編集用モーダル -->
+                             @include('modals.edit_todo')
+ 
+                             <!-- ToDoの削除用モーダル -->
+                             @include('modals.delete_todo')
+ 
+                             <div class="card mx-2 mb-2">
+                                 <div class="card-body">
+                                     <div class="d-flex justify-content-between align-items-center mb-2">
+                                         <h5 class="card-title ms-1 mb-0">{{ $todo->content }}</h5>
+                                         <div class="dropdown">
+                                             <a href="#" class="dropdown-toggle px-1 fs-5 fw-bold link-dark text-decoration-none menu-icon" id="dropdownTodoMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">︙</a>
+                                             <ul class="dropdown-menu dropdown-menu-end text-center" aria-labelledby="dropdownTodoMenuLink">                                                                                                                                                                                       
+                                                 <li><a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#editTodoModal{{ $todo->id }}">編集</a></li>                                                  
+                                                 <div class="dropdown-divider"></div>
+                                                 <li><a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deleteTodoModal{{ $todo->id }}">削除</a></li>  
+                                             </ul>
+                                         </div>
+                                     </div>   
+                                     <h6 class="card-subtitle ms-1 mb-1 text-muted">{{ $todo->created_at }}</h6>                                                               
+                                 </div>                                
+                             </div>
+                         @endforeach
                      </div>                           
                  </div>
              @endforeach                       
