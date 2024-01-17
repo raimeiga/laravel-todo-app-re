cb314700a4baeec5c75fd1a4a517800a1220b546
raimeiga <yusuke612@hotmail.com>
2024-01-17 12:52:48 +0900

    9.7 Todoコントローラの各アクションを編集

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,30 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use App\Models\Goal;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+    
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
+    
 
     /**
      * Store a newly created resource in storage.
@@ -34,32 +21,25 @@ class TodoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Goal $goal) {
+        //
+        $request->validate([
+            'content' => 'required',
+        ]);
+
+        $todo = new Todo();
+        $todo->content = $request->input('content');
+        $todo->user_id = Auth::id();
+        $todo->goal_id = $goal->id;
+        $todo->done = false;
+        $todo->save();        
+
+        return redirect()->route('goals.index');
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Todo $todo)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Todo $todo)
-    {
-        //
-    }
+    
 
     /**
      * Update the specified resource in storage.
@@ -68,9 +48,20 @@ class TodoController extends Controller
      * @param  \App\Models\Todo  $todo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Todo $todo)
-    {
-        //
+    
+    public function update(Request $request, Goal $goal, Todo $todo) {
+    
+        $request->validate([
+            'content' => 'required',
+        ]);
+
+        $todo->content = $request->input('content');
+        $todo->user_id = Auth::id();
+        $todo->goal_id = $goal->id;
+        $todo->done = $request->boolean('done', $todo->done);
+        $todo->save();
+
+        return redirect()->route('goals.index');     
     }
 
     /**
@@ -79,8 +70,11 @@ class TodoController extends Controller
      * @param  \App\Models\Todo  $todo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Todo $todo)
-    {
-        //
-    }
+    
+     public function destroy(Goal $goal, Todo $todo) {  
+    
+        $todo->delete();
+ 
+         return redirect()->route('goals.index');        
+     }
 }
