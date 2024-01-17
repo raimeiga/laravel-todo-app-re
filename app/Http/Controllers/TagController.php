a11b71e7fa3d645e5a71f19d3f17c048c5f8da2d
raimeiga <yusuke612@hotmail.com>
2024-01-17 13:32:40 +0900

    11.7 Tagコントローラの各アクションを作成

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -5,29 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Tag;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TagController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
+{   
     /**
      * Store a newly created resource in storage.
      *
@@ -36,29 +17,16 @@ class TagController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate([
+            'name' => 'required',
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Tag $tag)
-    {
-        //
-    }
+        $tag = new Tag();
+        $tag->name = $request->input('name');
+        $tag->user_id = Auth::id();
+        $tag->save();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Tag $tag)
-    {
-        //
+        return redirect()->route('goals.index');     
     }
 
     /**
@@ -70,7 +38,15 @@ class TagController extends Controller
      */
     public function update(Request $request, Tag $tag)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $tag->name = $request->input('name');
+        $tag->user_id = Auth::id();
+        $tag->save();
+
+        return redirect()->route('goals.index');   
     }
 
     /**
@@ -81,6 +57,8 @@ class TagController extends Controller
      */
     public function destroy(Tag $tag)
     {
-        //
+        $tag->delete();
+ 
+        return redirect()->route('goals.index'); 
     }
 }
